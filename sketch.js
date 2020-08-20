a84bc0a98f3ecc04a9a784486e1d8c938fa0ad3f
vj17 <varsha.jayaraj94@gmail.com>
2020-08-20 18:18:37 +0530

    Add chain and constrained bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,9 +17,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
     ground = new Ground(600,height,1200,20)
     platform = new Ground(150,305,300,170);
+
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -34,6 +36,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    log6 = new Log(230,180,80,PI/2);
+
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -42,6 +48,7 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+    
     box1.display();
     box2.display();
     ground.display();
@@ -58,5 +65,12 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    platform.display(); 
+
+    log6.display();
+    chain.display();
+
+
+
+   
 }
